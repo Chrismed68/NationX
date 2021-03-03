bb5f2e1f96e54196da93aff3754eb26bcbb9511b
Chris <chrismed68@gmail.com>
2021-03-03 18:44:38 -0500

    Edit 1
    Landscape Movement and camera

---
+++ Perg To NationX/Assets/Movement.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public CharacterController controller;
+    public Transform cam;
+
+    public float speed = 5.5f;
+
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVeloc;
+    // Update is called once per frame
+    void Update()
+    {
+        float hori = Input.GetAxisRaw("Horizontal");
+        float vert = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(hori, 0f, vert).normalized;
+        
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVeloc, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+    }
+}
---
+++ Perg To NationX/Assets/ponyaScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class ponyaScript : MonoBehaviour
+{
+
+    public GameObject ThePlayer;
+    public GameObject ponya;
+    public float TargetDistance;
+    public float AllowedDistance = 5;
+    public float FollowSpeed;
+    public RaycastHit Shot;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(ThePlayer.transform);
+        if(Physics.Raycast(transform.position,transform.TransformDirection(Vector3.forward), out Shot))
+        {
+            TargetDistance = Shot.distance;
+            if(TargetDistance >= AllowedDistance)
+            {
+                FollowSpeed = 0.1f;
+                transform.position = Vector2.MoveTowards(transform.position, ThePlayer.transform.position, FollowSpeed);
+            }
+            else 
+            {
+                FollowSpeed = 0;
+            }
+        }
+    }
+}
